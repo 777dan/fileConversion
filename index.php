d3a7b4bad7370b82120e4ed77e6b3163a3ec905c
777dan <danya.nikitin7@gmail.com>
2022-11-08 21:17:34 +0200

    v 2.0

--- index.php
+++ index.php
@@ -12,23 +12,68 @@ $strb = '<!DOCTYPE html>
 $str_e = '</body>
 </html>';
 $path = "new";
-for ($i = 0; $i < count($poemArr); $i++) {
-    // $file = fopen($poemArr[$i] . ".txt", "r");
-    $file = fopen($poemArr[$i] . ".txt", "w+");
-    fwrite($file, $poems[$i]);
-    $file = fopen($poemArr[$i] . ".txt", "r");
-    $content = fread($file, filesize($poemArr[$i] . ".txt"));
-    $html = fopen($path . "/" . $poemArr[$i] . ".html", "w");
-    fwrite($html, $strb);
-    fwrite($html, "\n");
-    fwrite($html, "<pre>");
-    fwrite($html, $content);
-    fwrite($html, "</pre>");
-}
-
 if (!is_dir($path)) {
     mkdir($path);
 }
+// for ($i = 0; $i < count($poemArr); $i++) {
+//     // $file = fopen($poemArr[$i] . ".txt", "r");
+//     $file = fopen($poemArr[$i] . ".txt", "w+");
+//     fwrite($file, $titles[$i]."\n");
+//     fwrite($file, $poems[$i]);
+//     $file = fopen($poemArr[$i] . ".txt", "r");
+//     $content = fread($file, filesize($poemArr[$i] . ".txt"));
+//     $strArr = explode("\n", $content);
+//     $firstStr = $strArr[0];
+//     $strArr[0] = "";
+//     $content = implode("\n", $strArr);
+//     // print_r($strArr);
+//     $html = fopen($path . "/" . $poemArr[$i] . ".html", "w");
+//     fwrite($html, $strb);
+//     fwrite($html, "\n");
+//     fwrite($html, "<h1>");
+//     fwrite($html, $firstStr);
+//     fwrite($html, "</h1>");
+//     fwrite($html, "\n");
+//     fwrite($html, "<pre>");
+//     fwrite($html, $content);
+//     fwrite($html, "</pre>");
+//     fwrite($html, $str_e);
+//     // print_r(scandir(""));
+// }
+$poemsPath = ".";
+
+if (is_dir($poemsPath)) {
+    $files = scandir($poemsPath, 1);
+    for ($i = 0; $i < count($files); $i++) {
+        if ($files[$i] != '.' && $files[$i] != '..' && $files[$i] != 'index.php' && $files[$i] != 'arrays.php' && $files[$i] != 'new' && $files[$i] != '.git') {
+            $filename = $poemsPath . "/" . $files[$i];
+            echo $filename . "\n";
+            $file = fopen($filename, "r");
+            $content = fread($file, filesize($filename));
+            $strArr = explode("\n", $content);
+            $firstStr = $strArr[0];
+            $strArr[0] = "";
+            $content = implode("\n", $strArr);
+            $new_filename = pathinfo($files[$i], PATHINFO_FILENAME);
+            $html = fopen($path . "/" . $new_filename . ".html", "w");
+            fwrite($html, $strb);
+            fwrite($html, "\n");
+            fwrite($html, "<h1>");
+            fwrite($html, $firstStr);
+            fwrite($html, "</h1>");
+            fwrite($html, "\n");
+            fwrite($html, "<pre>");
+            fwrite($html, $content);
+            fwrite($html, "</pre>");
+            fwrite($html, "\n");
+            fwrite($html, $str_e);
+        }
+    }
+}
+
+// if (!is_dir($path)) {
+//     mkdir($path);
+// }
 echo realpath($path);
 echo "\n";
 echo getcwd();
